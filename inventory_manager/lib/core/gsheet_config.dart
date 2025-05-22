f94b2db70fd5de8a11074deb6a184c08b876a4f1
Tapoban Ray <raytapoban@gmail.com>
2025-05-22 18:43:28 +0530

    Connected gsheets with app

---
+++ inventory_manager/lib/core/gsheet_config.dart
@@ -0,0 +1,15 @@
+import 'package:gsheets/gsheets.dart';
+import 'package:inventory_manager/gsheet_setup.dart' show credentials, sheetId;
+
+final gSheetInit = GSheets(credentials);
+
+Spreadsheet? gSheetController;
+
+Worksheet? gSheetUserDetails;
+
+Future<void> gSheetInitialise() async {
+  gSheetController = await gSheetInit.spreadsheet(sheetId);
+  gSheetUserDetails = await gSheetController?.worksheetByTitle(
+    "composition_sheet",
+  );
+}
